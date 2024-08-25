d8786c5c1a299fb79a174326a71723003822f22f
akkiee-daiki <daikiakibayashi@aol.com>
2024-08-25 14:01:29 +0900

    create Home component

--- learning-time-register/src/App.js
+++ learning-time-register/src/App.js
@@ -1,7 +1,9 @@
-
+import { Home } from "./Home";
 function App() {
   return (
     <div className="App">
+      <h1>学習時間記録アプリ</h1>
+      <Home />
     </div>
   );
 }
---
+++ learning-time-register/src/Home.jsx
@@ -0,0 +1,9 @@
+import { Form } from "./Form";
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Form />
+    </div>
+  );
+};
